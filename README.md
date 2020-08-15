f21591923c18e89d9e1be2b01c303dd13e039b42
Clark Cooper <clark.cooper@gmail.com>
2020-08-15 19:29:49 -0400

    This is a working but ugly puzzle.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ShiftMerge-Puzzle
-ShiftMerge is a puzzle written in Tcl/Tk similar to 2048 by Gabriele Cirulli and Threes by Asher Vollmer
+ShiftMerge is a puzzle written in Tcl/Tk similar to 2048 by Gabriele Cirulli
+and Threes by Asher Vollmer
+
+## Introduction
+
+This project was initiated to teach myself Tcl/Tk, with which I plan to implement
+a GUI on another project. So until it matures, don't expect it to be an
+example of good Tcl/Tk usage.
+
+I chose Cirulli's 2048 as a model since I've enjoyed it as a light weight
+pastime when I'm waiting for something. and it won't require me to learn
+anything else but Tcl/Tk itself. With that game, I'd always wanted a
+way to undo my last move, so I'm aiming to provide that here. It would also be
+fun to experiment with the size of the field, the relative frequency of the
+4 vs the 2 for insertions, and other modes of play. In case of different modes
+of play, they will all have these features in common:
+
+- a square grid of cells that are either empty or contain compatible elements
+- arrow keys shift elements into empty cells in the direction of the arrow,
+leaving all empty spaces on the side of the grid opposite to the shift
+- if, after a shift, adjacent non-empty elements meet conditions for the mode,
+they will be merged and trailing elements will shifted into the resulting
+empty cell(s)
+- when the grid has no empty cells and no shift will merge anything, the
+game is over
+
+## Requirements
+You will need Tcl/Tk installed on your system. This script was developed
+using versions 8.6 of both Tcl and Tk.
---
+++ shiftmerge.tcl
@@ -0,0 +1,204 @@
+#!/usr/bin/wish
+
+package require Tk
+
+#================================================================
+# Procedure definitions
+
+# True if there are no empty cells
+proc full {{gridname cell}} {
+    upvar 1 $gridname c
+    set ckeys [array names c]
+    set emptycnt 0
+    
+    foreach k $ckeys {
+	if {$c($k) eq {}} {incr emptycnt}
+    }
+
+    return [expr {! $emptycnt}]
+}
+
+# insert 2 or 4 randomly into cells
+proc insert {{gridname cell}} {
+    upvar 1 $gridname c
+    set empty {}
+    foreach {k v} [array get c] {
+	if {$v eq {}} {
+	    lappend empty $k
+	}
+    }
+
+    set cnt [llength $empty]
+
+    if {! $cnt} {error "Attempt to insert into a full grid"}
+
+    set newnum [if {int(rand()*10)} {expr 2} {expr 4}]
+    set empndx [expr {int(rand()*$cnt)}]
+    set cellndx [lindex $empty $empndx]
+    
+    set c($cellndx) $newnum
+    return [expr $cnt - 1]
+}
+
+proc clear {} {
+    global cell
+    foreach k [array names cell] {
+	set cell($k) {}
+    }
+}
+
+# Return a true value if grid is not full or if some shift will
+# cause 1 or more merges so that the grid would no longer be full
+proc playable {} {
+    global cell
+    if {! [full]} {
+	return 1
+    } else {
+	array set tstcell [array get cell]
+
+	#note that for a non-playable grid NOTHING changes under shift
+	foreach d {Up Down Left Right} {
+	    shift $d tstcell
+	    if {! [full tstcell]} {return 1}
+	}
+
+	return 0
+    }
+}
+
+proc bindkeys {{gameover 0}} {
+    global msg
+    
+    if {$gameover} {
+	bind . <<arrows>>   {set msg "Click Start New Game"}
+    } else {
+	bind . <<arrows>> {move %K}
+    }
+    bind . <BackSpace> {set msg "Undo is not yet implemented"}
+}
+
+
+proc shift {d {gridname cell}} {
+    upvar 1 $gridname c
+    switch $d {
+	Up {
+	    set cktemplate {0,$i 1,$i 2,$i 3,$i}
+	}
+
+	Down {
+	    set cktemplate {3,$i 2,$i 1,$i 0,$i}
+	}
+
+	Left {
+	    set cktemplate {$i,0 $i,1 $i,2 $i,3}
+	}
+
+	Right {
+	    set cktemplate {$i,3 $i,2 $i,1 $i,0}
+	}
+    }
+
+    set changes 0
+
+    for {set i 0} {$i < 4} {incr i} {
+	set ckeys [subst $cktemplate]
+
+	set vals {}
+	set merge 0
+	
+	# number of changed rows or columns
+	
+	foreach k $ckeys {
+	    set v $c($k)
+	    if {$v eq {}} {continue}
+	    if {$v eq [lindex $vals end]
+	        && ! $merge} {
+		lset vals end [expr 2*$v]
+		set merge 1
+	    } else {
+		lappend vals $v
+		set merge 0
+	    }
+	}
+
+	set vcnt [llength $vals]
+	
+	# if vals is empty or full, there's nothing to change for this row/col
+	if {$vcnt == 0 || $vcnt == 4} {continue}
+
+	incr changes
+
+	# pad to 4
+	while {[llength $vals] < 4} {
+	    lappend vals {}
+	}
+
+	set csetlist {}
+	
+	for {set j 0} {$j < 4} {incr j} {
+	    lappend csetlist [lindex $ckeys $j] [lindex $vals $j]
+	}
+
+	array set c $csetlist
+    }
+
+    return $changes
+}
+
+proc move {d} {
+    global msg
+    global cell
+
+    if {[shift $d] == 0} {set msg "shift $d didn't do anything"; return}
+
+    set msg {}
+    
+    if {! [full]} {insert}
+
+    if {! [playable]} {
+	set msg "Game over"
+	bindkeys 1
+    }
+}
+
+proc restart {} {
+    global msg
+    global cell
+
+    set msg {}
+    clear
+    insert; insert
+    bindkeys
+}
+
+#================================================================
+# Layout
+
+wm title . "Shift-Merge"
+pack [frame .f -padx 5 -pady 5]
+pack [label .f.inst -text "Use arrow keys to shift"]
+
+pack [frame .f.tab4 -bg gray80 -bd 2 -relief solid]
+
+font create CellFont -family "Comic Sans MS" -size 20
+
+for {set i 0} {$i < 4} {incr i} {
+    for {set j 0} {$j < 4} {incr j} {
+	set ndx "$i,$j"
+	set cname ".f.tab4.cell$i$j"
+	set cell($ndx) ""
+	label $cname -textvariable cell($ndx) -bg LightYellow -font CellFont \
+	    -width 5 -height 2 -anchor center
+	grid $cname -row $i -column $j
+    }
+}
+
+grid rowconfigure .f.tab4 all -minsize 100
+grid columnconfigure .f.tab4  all -minsize 100
+
+pack [label .f.msg -textvariable msg -width 60] -side left
+pack [button .f.but -text "Start New Game" -command restart] -side right
+
+event add <<arrows>> <Up> <Down> <Left> <Right>
+
+restart
